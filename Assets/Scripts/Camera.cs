749570fe88f6cda49e954ccde4b03050dfd758c4
Killigo <Killigo@mail.ru>
2023-02-21 21:09:50 +0300

    fix
    Исправлено:
    Transform и GameObject это два основных компонента Unity, ссылки на которые всегда есть у объекта и для доступа к ним есть соответствующие свойства transform и gameObject
    
    Дмитрий Прокопьев

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,18 +4,12 @@ public class Camera : MonoBehaviour
 {
     [SerializeField] private Player _player;
 
-    private Transform _playerTransform;
     private Vector3 _position;
     private float _zCameraPosition = -10f;
 
-    private void Start()
-    {
-        _playerTransform = _player.GetComponent<Transform>();
-    }
-
     private void Update()
     {
-        _position = _playerTransform.position;
+        _position = _player.transform.position;
         _position.z = _zCameraPosition;
         transform.position = Vector3.Lerp(transform.position, _position, Time.deltaTime);
     }
